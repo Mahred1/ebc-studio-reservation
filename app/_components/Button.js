f4e3a3c389446d182e07fea24c856c7bbb8c96e9
Mahred1 <mredwan.kemal@gmail.com>
2025-07-11 21:48:45 +0300

    finished  creating the home page

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -4,8 +4,8 @@ function Button({ children, type = "primary" }) {
       className={`${type == "primary" && "bg-primary-100 text-white"} ${
         type === "secondary" && "bg-secondary-100 text-primary-100"
       } ${
-        type==="action"&&"bg-primary-200 text-white"
-      } px-10 py-3 font-md rounded-md font-light tracking-widest `}
+        type === "action" && "bg-primary-200 text-white"
+      } px-10 py-3 font-md rounded-md font-semibold tracking-widest hover:scale-[103%] `}
     >
       {children}
     </button>
--- app/page.js
+++ app/page.js
@@ -1,16 +1,32 @@
 import Image from "next/image";
 import Button from "./_components/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col items-center justify-center ">
       <div>
-      <Image width={285} height={285} src="/ebc-logo.jpg" alt="EBC" />
+        <Image
+          className="self-center mb-5  "
+          width={250}
+          height={250}
+          src="/ebc-logo.jpg"
+          alt="EBC"
+        />
       </div>
-      <h1>Welcome to <span>EBC</span></h1>
-      <p>Rent the Best Studio – Elevate Your Production Today!</p>
-      <div>
-        <Button>Reserve a studio</Button>
+      <h1 className="font-medium text-4xl tracking-wider">
+        Welcome to <span className="text-primary-100 font-bold">EBC</span>
+      </h1>
+      <p className="mt-0.5 text-xl font-normal ">
+        Rent the Best Studio – Elevate Your Production Today!
+      </p>
+      <div className="flex gap-10 mt-7 mb-[130px]">
+        <Link href={"/reserve"}>
+          <Button>Reserve a studio</Button>
+        </Link>
+        <Link href={'/check'}>
+          <Button type="secondary">Check Reservation</Button>
+        </Link>
       </div>
     </main>
   );
