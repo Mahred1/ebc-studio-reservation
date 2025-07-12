9fb98d6ba9c8d2188b34c19e7a7939626e7c089e
Mahred1 <mredwan.kemal@gmail.com>
2025-07-12 10:01:54 +0300

    checkbox

---
+++ app/_components/Checkbox.js
@@ -0,0 +1,34 @@
+function Checkbox() {
+  return (
+    <div className="flex items-center gap-4 my-5">
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          id="local"
+          name="local"
+          class="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+        />
+        <label
+          for="terms"
+          class="ml-2 block text-md font-semibold text-gray-900"
+        >
+          Local
+        </label>
+      </div>
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          id="worldwide"
+          name="worldwide"
+          class="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+        />
+        <label
+          for="terms"
+          class="ml-2 block text-md font-semibold text-gray-900"
+        >
+          Worldwide
+        </label>
+      </div>
+    </div>
+  );
+}
--- app/reserve/page.js
+++ app/reserve/page.js
@@ -17,39 +17,12 @@ function Page() {
       </h1>
 
       <form className="flex flex-col items-center">
-        <Checkbox />
+        
       </form>
     </div>
   );
 }
 
-function Checkbox() {
-  return (
-    <div className="flex items-center gap-4 my-5">
-      <div className="flex items-center">
-        <input
-          type="checkbox"
-          id="local"
-          name="local"
-          class="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-        />
-        <label for="terms" class="ml-2 block text-md font-semibold text-gray-900">
-          Local
-        </label>
-      </div>
-      <div className="flex items-center">
-        <input
-          type="checkbox"
-          id="worldwide"
-          name="worldwide"
-          class="h-5 w-5 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-        />
-        <label for="terms" class="ml-2 block text-md font-semibold text-gray-900">
-          Worldwide
-        </label>
-      </div>
-    </div>
-  );
-}
+
 
 export default Page;
