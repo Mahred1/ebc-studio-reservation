db00c65e7935856627e9e100bbfbec06da5075a3
Mahred1 <mredwan.kemal@gmail.com>
2025-07-14 13:32:53 +0300

    updated env variables

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hfosufqskzimezdvnfrb.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmb3N1ZnFza3ppbWV6ZHZuZnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMTc4ODMsImV4cCI6MjA2Nzg5Mzg4M30.wri-vYlOIWn-DeDVA1eTsLgA83ZAswD4l-IjBKaaeFw";
+const supabaseUrl =process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey =process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
